e5788158577fb6ddff18c6882dc00250dc490f45
raghav-rama <hackerer528@gmail.com>
2024-01-27 14:18:01 +0530

    fix: minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import ConnectButton from "@/components/connect-wallet-btn";
+import Navbar from "@/components/nav-bar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ConnectButton />
+    <main className="flex min-h-screen">
+      {/* <ConnectButton /> */}
+      <Navbar />
     </main>
   );
 }
--- src/components/connect-wallet-btn.tsx
+++ src/components/connect-wallet-btn.tsx
@@ -1,28 +1,11 @@
 "use client";
 import { useWeb3Modal } from "@web3modal/wagmi1/react";
-import { useSignMessage } from "wagmi";
 
 export default function ConnectButton() {
-  // 4. Use modal hook
-  //   const { open } = useWeb3Modal();
-  const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage({
-    message: "gm wagmi frens",
-  });
-
+  const { open } = useWeb3Modal();
   return (
     <>
-      {/* <button onClick={() => open()}>Open Connect Modal</button>
-      <button onClick={() => open({ view: "Networks" })}>
-        Open Network Modal
-      </button> */}
-      <w3m-button />
-      <div>
-        <button disabled={isLoading} onClick={() => signMessage()}>
-          Sign message
-        </button>
-        {isSuccess && <div>Signature: {data}</div>}
-        {isError && <div>Error signing message</div>}
-      </div>
+      <button onClick={() => open()}>Connect Wallet</button>
     </>
   );
 }
