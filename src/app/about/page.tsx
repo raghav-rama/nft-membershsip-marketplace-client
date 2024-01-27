340ec817831643a409b4f5224c04d3583e80474b
raghav-rama <hackerer528@gmail.com>
2024-01-27 17:44:05 +0530

    feat: add initial routes for settings and about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import isAuth from "@/components/is-auth";
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+}
+export default isAuth(About);
---
+++ src/app/settings/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import isAuth from "@/components/is-auth";
+
+function Settings() {
+  return (
+    <div>
+      <h1>Settings</h1>
+    </div>
+  );
+}
+export default isAuth(Settings);
